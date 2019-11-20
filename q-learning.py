ce4c0cad8f22c47b56bbee44d9f3af34ddc1bb89
Tons-of-Carls <carlsim66@yahoo.com>
2019-11-19 23:16:46 -0800

    seems to be working much better, added in a graph that will display how the agent improves over time

--- q-learning.py
+++ q-learning.py
@@ -1,113 +1,83 @@
 import gym
+import gym.envs.toy_text.frozen_lake as fl
 import numpy as np
+import matplotlib.pyplot as plt
 
 # environment
-env = gym.make('FrozenLake-v0')
+env = gym.make('FrozenLake-v0')#, is_slippery=False)
 env.reset()
 
-QFunction = np.zeros((16, 4))
-alpha = 0.05
-gama = 0.9
+QFunction = np.zeros((env.nS, env.nA))
+alpha = 0.01
+gamma = 0.9
+epsilon = 0.2
 
-
-# Training
-state = 0
+state = env.s
 reward = 0
 done = False
 info = {}
 
 goal_found = 0
+accuracy = []
+
 
 # Gets the next action to take given the current state
 # Uses gama to decide when to randomly select an action (Explore) and when to select based on Q value (Exploit)
-def get_action(obs, best = False):
-    if best or np.random.random() < gama:
-        # randomly chooses action to do if Q values are all the same
-        return np.random.choice(np.flatnonzero(QFunction[obs] == np.max(QFunction[obs])))
-    return np.random.randint(0, 4)
+def get_action(s, best = False):
+    if best or np.random.random() < epsilon:
+        return np.argmax(QFunction[s])
+    return np.random.randint(0, env.action_space.n)
 
 
-for i in range(7000):
+def simulate(training = True):
+    global state, reward, done, info, goal_found
     while True:
         env.render()
 
-        # stores current observation so it can be used to calculate update to Q function
-        oldObservation = state
+        # stores current state so it can be used to calculate update to Q function
+        old_state = state
 
-        # do some action
-        action = get_action(oldObservation)
+        # do some action, if we are testing always use q-function to get best move
+        action = get_action(old_state, best=not training)
         state, reward, done, info = env.step(action)
-
-        # Update Q value
-        QFunction[oldObservation, action] = (1-alpha)*QFunction[oldObservation, action] + alpha * (reward + gama * QFunction[state, get_action(state, best=True)])
+        if training:
+            # Update Q value
+            # i think there is something wrong with my update function
+            QFunction[old_state, action] = (1 - alpha)*QFunction[old_state, action] + alpha * (reward + gamma * QFunction[state, get_action(state, best=True)])
 
         # stop condition
         if done:
             if reward == 1:
                 goal_found += 1
-            QFunction[state, 0] = reward
-            QFunction[state, 1] = reward
-            QFunction[state, 2] = reward
-            QFunction[state, 3] = reward
-            env.reset()
             break
 
+
+
     # reset variables run game again
-    observation = 0
+    env.reset()
+    state = env.s
     reward = 0
     done = False
     info = {}
 
-print(goal_found)
-print("Left down right up")
-print(QFunction)
 
+# Training
+for i in range(10000):
+    simulate()
+    if i % 100 == 0:
+        # test
+        goal_found = 0
+        for j in range(100):
+            simulate(training=False)
+        accuracy.append([i/100, goal_found/100])
 
-# Testing
-goal_found2 = 0
-
-observation = 0
-reward = 0
-done = False
-info = {}
-
-env.reset()
-
-for i in range(1000):
-    while True:
-        env.render()
-
-        # stores current observation so it can be used to calculate update to Q function
-        oldObservation = state
-
-        # do some action
-        action = get_action(oldObservation)
-        state, reward, done, info = env.step(action)
-
-        # stop condition
-        if done:
-            if reward == 1:
-                goal_found2 += 1
-            QFunction[state, 0] = reward
-            QFunction[state, 1] = reward
-            QFunction[state, 2] = reward
-            QFunction[state, 3] = reward
-            env.reset()
-            break
-
-    # reset variables run game again
-    observation = 0
-    reward = 0
-    done = False
-    info = {}
-
-print(goal_found2)
+accuracy = np.matrix(accuracy)
+plt.plot(accuracy[:, 0], accuracy[:, 1])
+plt.show()
 
 env.close()
 
 # Jointly optimized:
-# maze where certain walls can be removed when an agent is standing on a certain square
-# How woudl states be encoded? seem like a lot of states, actions would be
-
-# Separately optimized:
+# chess where one action determines which piece to move and the other action where to move that piece
 #
+
