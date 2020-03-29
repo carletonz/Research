d155d03edb2b854285caaae29bdca0b22d916ce8
carletonz <carletoz@uci.edu>
2020-03-29 08:55:02 +0000

    started creating tensorflow verison of moe, finished env still need to test

---
+++ MOE/tf/moe_core_tf.py
@@ -0,0 +1,161 @@
+# this is based off of:
+# https://royf.org/pub/pdf/Yan2019Multi.pdf
+# https://github.com/AndyLc/mtl-multi-clustering
+
+import tensorflow as tf
+from tensorflow import keras
+
+M = 10 # Number of experts
+N = 5 # Number of tasks
+CONNECTION_SIZE = 10 # output size of expert and input size of task head
+
+# expert where inputs are color images
+# (f)
+def Expert_conv(input_shape):
+    x = keras.Input(shape=input_shape)
+    conv1 = keras.layers.Conv2D(6, 5, activation="relu")(x)
+    pool1 = keras.layers.MaxPooling2D((2,2), 2)(conv1)
+    conv2 = keras.layers.Conv2D(16, 5, activation="relu")(pool1)
+    pool2 = keras.layers.MaxPooling2D((2,2), 2)(conv2)
+    
+    flatten = keras.layers.Flatten()(pool2)
+    
+    fc1 = keras.layers.Dense(120)(flatten)
+    fc2 = keras.layers.Dense(84)(fc1)
+    fc3 = keras.layers.Dense(CONNECTION_SIZE)(fc2)
+    
+    return keras.Model(x, fc3)
+
+# experts where inputs are vectors
+# (f)
+def Expert_linear(input_shape):
+    x = keras.Input(shape=input_shape)
+    
+    hl1 = keras.layers.Dense(120)(x)
+    hl2 = keras.layers.Dense(84)(fc1)
+    hl3 = keras.layers.Dense(CONNECTION_SIZE)(fc2)
+    
+    return keras.Model(x, fc3)
+
+# Decide to use a specific expert on a task
+# Weights decide how much each expert matters
+# (b / w)
+class Gating(nn.Module):
+    def __init__(self):
+        super(Gating, self).__init__()
+        self.weights = nn.Parameter(torch.zeros([M, N]))
+        self.logits = nn.Parameter(torch.zeros([M, N]))
+    
+    def forward(self, x, extra_loss):
+        """
+        :param x: outputs from experts B x M x F
+        :param extra_loss
+        :return B x N x F : 
+        """
+        bernoulli = torch.distributions.bernoulli.Bernoulli(logits=self.logits)
+        
+        b = bernoulli.sample()
+        w = self.weights * b
+        # depeds on b
+        # should be a funcition for log probs
+        logits_loss = torch.sum(bernoulli.log_prob(b), 0)
+        
+        #outer product, sum
+        output = torch.einsum('mn, bmf->bnf', w, x)# need to be all the same type
+        return output, extra_loss + logits_loss
+
+# Task Head for each task
+# 'a' is the size of the output space for each task
+# (g)
+def Task(input_shape):
+    x = keras.Input(shape=input_shape)
+    
+    hl1 = keras.layers.Dense(120)(x)
+    hl2 = keras.layers.Dense(84)(fc1)
+    hl3 = keras.layers.Dense(CONNECTION_SIZE)(fc2)
+    
+    return keras.Model(x, fc3)
+
+# (m)
+class MixtureOfExperts(nn.Module):
+    def __init__(self, input_size, task_output_size, input_type="V"):
+        super(MixtureOfExperts, self).__init__()
+        self.experts = None
+        self.expert_type = None
+        if input_type == "I": # image
+            self.experts = nn.ModuleList([
+                Expert_conv() for i in range(M)
+            ])
+            self.expert_type = 0
+        elif input_type == "V": # vector
+            self.experts = nn.ModuleList([
+                Expert_linear(input_size) for i in range(M)
+            ])
+            self.expert_type = 1
+        else:
+            raise ValueError()
+        
+        self.gates = Gating()
+        self.taskHeads = nn.ModuleList([
+                Task(task_output_size[i]) for i in range(N)
+            ])
+        
+        self.train = True
+    
+    def forward(self, x):
+        """
+        :param x: tensor containing a batch of images / states / observations / etc.
+        :param task: tensor containing a task number
+        """
+        # makes sure experts are batched and have correct number of input dimentions
+        if self.expert_type == 0 and len(x.shape) != 3: # Images
+            raise ValueError("Expecting batched images, got {}".format(x.shape))
+        if self.expert_type == 1 and len(x.shape) != 2: # Vectors
+            raise ValueError("Expecting batched vectors, got {}".format(x.shape))
+        
+        B = x.shape[0] # batch size
+        
+        # in: B x width x height
+        # out: B x M x F
+        expert_output = torch.stack([self.experts[i](x) for i in range(M)], dim = 1)
+        
+        # in: B x M x F
+        # out: B x N x F
+        self.cumulative_logits_loss = torch.zeros(N)
+        gates_output, self.cumulative_logits_loss = self.gates(expert_output, self.cumulative_logits_loss)
+        
+        # in: B x N x F
+        # out: B x O
+        # O is the sum of all task output sizes
+        # cancatinate this so all task outputs are in the same dimention
+        taskHead_output = torch.cat([self.taskHeads[i](gates_output[:,i]) for i in range(N)], dim=1)
+        return taskHead_output
+    
+    def get_loss(self, loss):
+        logits_loss = self.cumulative_logits_loss * loss.detach()
+        total_loss = logits_loss + loss
+        return total_loss.sum()
+
+
+if __name__ == "__main__":
+    input_type = "V"
+    task_output_size = list(range(N))
+    input_size = 20
+    batches = 50
+    output_size = int(sum(task_output_size))
+    
+    moe = MixtureOfExperts(input_size, task_output_size, input_type)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(moe.parameters(), lr=0.001, momentum=0.9)
+    
+    for i in range(5):
+        inputs = torch.randn(batches, input_size)
+        label = torch.randint(0, output_size, (batches,))
+
+        output = moe(inputs)
+        #output, action = torch.max(output, 0)
+        optimizer.zero_grad()
+        loss = criterion(output, label)
+        loss = moe.get_loss(loss)
+        loss.backward()
+        optimizer.step()
--- MOE/utils.py
+++ MOE/utils.py
@@ -1,33 +1,45 @@
-from gym import Env
+from gym import spaces
+import gym
+import numpy as np
 
-# set of observation or action spaces
-class SpaceSet:
-    def __init__(self, envs):
-        pass
 # set of environments
-class EnvSet(Env):
+class EnvSet(gym.Env):
     def __init__(self, env_fns):
         self.envs = [env_fns[i]() for i in range(len(env_fns))]
+        action_space_low = np.concatenate([e.action_space.low for e in self.envs])
+        action_space_high = np.concatenate([e.action_space.high for e in self.envs])
+        self.action_splits = np.concatenate([e.action_space.shape for e in self.envs])
         
-        self.action_space = None
-        self.observation_space = None
-        self.reward_range = None
+        obs_space_low = np.concatenate([e.observation_space.low for e in self.envs])
+        obs_space_high = np.concatenate([e.observation_space.high for e in self.envs])
+        obs_splits = np.concatenate([e.observation_space.shape for e in self.envs])
         
-    def step(self):
-        pass
+        self.action_space = spaces.Box(action_space_low, action_space_high)
+        self.observation_space = spaces.Box(obs_space_low, obs_space_high)
+        
+    def step(self, action):
+        # List of actions separated by environment
+        a = torch.split(action, self.action_splits)
+        
+        #List of observation, reward, done, info tuple for each environment
+        env_state = [self.envs[i].step(a[i]) for i in range(len(self.envs))]
+        
+        obs = np.concatenate([env_state[i][0] for i in range(len(env_state))])
+        reward = np.concatenate([env_state[i][1] for i in range(len(env_state))]).sum()
+        done = np.all([env_state[i][2] for i in range(len(env_state))])
+        info = [env_state[i][3] for i in range(len(env_state))]
+        
+        return obs, reward, done, info
     
     def reset(self):
-        return [env.reset() for env in self.envs]
+        return np.concatenate([env.reset() for env in self.envs])
 
     def render(self):
-        pass
+        for env in self.envs:
+            env.render()
     
     def close(self):
         pass
     
     def seed(self):
         pass
-
-def temp():
-    return 1
-EnvSet([temp])
